38f0f0709e065cc450738c5bd45bac558b499610
ragini.patidar <97217996+Ragini-Patidar@users.noreply.github.com>
2023-10-17 11:08:36 +0530

    Adding more vidoes

--- src/mocks/components/blickBites/index.ts
+++ src/mocks/components/blickBites/index.ts
@@ -18,5 +18,23 @@ export const blickBitesMocks: ListProps[] = [
     text: "Apple",
     videoUrl: "https://cdn.jwplayer.com/v2/playlists/nYV6v3Sc?format=json",
     isCurrent: false,
+  },
+  {
+    image: `${process.env.PUBLIC_URL}/assets/image4.png`,
+    text: "Video 4",
+    videoUrl: "https://cdn.jwplayer.com/v2/playlists/dUcp13Y3?format=json",
+    isCurrent: false,
+  },
+  {
+    image: `${process.env.PUBLIC_URL}/assets/image5.png`,
+    text: "Video 5",
+    videoUrl: "https://cdn.jwplayer.com/v2/playlists/2FeMGSFz?format=json",
+    isCurrent: false,
+  },
+  {
+    image: `${process.env.PUBLIC_URL}/assets/image6.png`,
+    text: "Video 6",
+    videoUrl: "https://cdn.jwplayer.com/v2/playlists/9pjBkInZ?format=json",
+    isCurrent: false,
   }
 ]
